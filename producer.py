f85d72884409d1aa70ba22598668148711ccb3e7
Hyunwoo Hong <zusdn90@naver.com>
2022-10-12 16:45:31 +0900

    Update producer.py
    producer 데이터 전송 시 key,value 형태로 전송

--- producer.py
+++ producer.py
@@ -5,14 +5,14 @@ import time
 producer = KafkaProducer(acks=0,
                          compression_type='gzip',
                          bootstrap_servers=['13.114.187.232:9092'],
+                         key_serializer=str.encode,
                          value_serializer=lambda x: dumps(x).encode('utf-8')
                          )
 
 start = time.time()
 
 for i in range(10000):
-    data = {'str': 'result' + str(i)}
-    producer.send('test', value=data)
+    producer.send('test', key= 'key_' + str(i), value='value_' + str(i))
     producer.flush()
 
 print("elapsed :", time.time() - start)
